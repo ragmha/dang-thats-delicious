cce58d7ade2db4587cd05d491e0abe34a6996f43
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-09-09 18:09:33 +0300

    add mapStore endpoint

--- routes/index.js
+++ routes/index.js
@@ -56,5 +56,6 @@ router.post(
 
 // API
 router.get('/api/search', catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 module.exports = router;
