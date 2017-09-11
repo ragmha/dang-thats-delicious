0c8bfbfab5610d1edc44a23ba2595de481d3c0ee
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-09-11 11:50:46 +0300

    add pagination :D

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ router.post(
 );
 
 router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/page/:page', catchErrors(storeController.getStores));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
